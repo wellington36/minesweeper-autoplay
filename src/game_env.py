c1699e76cbd436f6f6ee92b4ace8788215440461
André <andrerg02@gmail.com>
2023-10-31 18:27:11 -0300

    Moving from gym to gymnasium; fixing warnings

--- src/game_env.py
+++ src/game_env.py
@@ -1,11 +1,9 @@
-import gym
+import gymnasium as gym
 import pygame
 import numpy as np
-from gym import spaces
-from game import Game as Minesweeper
-
-MINES = 80
-GRID_SIZE = 20
+from gymnasium import spaces
+from gymnasium.envs.registration import register
+from game import Game as Minesweeper, GRID_SIZE, MINES
 
 class MinesweeperEnv(gym.Env):
     def __init__(self, graphics=True):
@@ -14,51 +12,52 @@ class MinesweeperEnv(gym.Env):
         self.minesweeper = Minesweeper(graphics=graphics)
 
         self.action_space = spaces.Discrete(GRID_SIZE**2)
-        self.observation_space = spaces.Box(low=0, high=8, shape=(GRID_SIZE, GRID_SIZE), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=8, shape=(GRID_SIZE * GRID_SIZE,), dtype=np.uint8)  # Flatten the observation space
 
-    def reset(self):
+    def reset(self, seed=None, options=None):
         self.minesweeper = Minesweeper(graphics=self.graphics)
         self.reward = 0  # Initialize the reward to zero
-        return self.get_state()
+        return self.get_state(), {}
 
     def step(self, action):
-        x, y = action % GRID_SIZE, action // GRID_SIZE
-        self.minesweeper.on_click((x, y))
-        state = self.get_state()
-        done = self.minesweeper.explosion or len(self.minesweeper.checked) == GRID_SIZE ** 2 - MINES
+     x, y = action % GRID_SIZE, action // GRID_SIZE
+     self.minesweeper.on_click((x, y))
+     state = self.get_state()
+     done = self.minesweeper.explosion or len(self.minesweeper.checked) == GRID_SIZE ** 2 - MINES
 
-        if done:
-            if self.minesweeper.explosion:
-                self.reward -= 1  # Lose
-            else:
-                self.reward += 1  # Win
-        else:
-            # You win +0.1 for each non-mine tile clicked
-            if self.minesweeper.mines[x,y] == 0:
-                self.reward += 0.1
+     if done:
+         if self.minesweeper.explosion:
+             self.reward -= 1  # Lose
+         else:
+             self.reward += 1  # Win
+     else:
+         # You win +0.1 for each non-mine tile clicked
+         if self.minesweeper.mines[x, y] == 0:
+             self.reward += 0.1
 
-        return state, self.reward, done, {}
+     return state, self.reward, done, False, {}  # Return a tuple of (observation, reward, done, info)
 
     def render(self):
         if self.graphics:
             self.minesweeper.draw()
 
     def get_state(self):
-        state = np.zeros((GRID_SIZE, GRID_SIZE), dtype=np.uint8)
+        state = np.zeros(GRID_SIZE * GRID_SIZE, dtype=np.uint8)
         for pos in self.minesweeper.checked:
-            state[pos] = self.minesweeper.check_mines(pos)
+            state[pos[0] * GRID_SIZE + pos[1]] = self.minesweeper.check_mines(pos)
         return state
 
-env = MinesweeperEnv(graphics=True)  # You can set graphics to False to run without visuals
+register(
+    id='MineSweeper-v0',
+    entry_point='game_env:MinesweeperEnv', 
+)
 
-obs = env.reset()
+env = gym.make('MineSweeper-v0', graphics=True)  # You can set graphics to False to run without visuals
+
+obs, _ = env.reset(seed=42, options=None)
 done = False
-for i in range(100):
-    while not done:
-        action = env.action_space.sample()  # Replace with your own action selection logic
-        obs, reward, done, _ = env.step(action)
-        print(action, reward, done)
-        env.render()
-        pygame.time.wait(100)
-    obs = env.reset()
-    done = False
+while not done:
+    action = env.action_space.sample()  # Replace with your own action selection logic
+    obs, reward, done, _, _ = env.step(action)
+    env.render()
+    pygame.time.wait(100)
