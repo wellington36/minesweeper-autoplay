7152af233073c0ca8061e318f107a1d870464459
André <andrerg02@gmail.com>
2023-10-31 18:29:57 -0300

    Adding Monitor class to fix another warning

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
@@ -9,6 +10,7 @@ from game_env import MinesweeperEnv
 def train_minesweeper():
     # Create and wrap the environment
     env = MinesweeperEnv(graphics=False)
+    env = Monitor(env)
     env = DummyVecEnv([lambda: env])
 
     # Create a PPO model
@@ -18,11 +20,10 @@ def train_minesweeper():
     model.learn(total_timesteps=int(1e4))  # Adjust the number of training steps as needed
 
     # Evaluate the model
-    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
+    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=1)
     print(f"Mean reward: {mean_reward}")
 
     # Save the trained model
-    #model.save("C:\\Users\\andre\\Desktop\\galaxypy-autoplay\\saved-models")       # Andres
     model.save("../saved-models/minesweeper_model")                                 # Wellington
 
 if __name__ == "__main__":
