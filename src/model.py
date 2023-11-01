e124106b485058bee1e04e1843a0bf236be29411
Wellington36 <wellington.71319@gmail.com>
2023-10-31 22:28:27 -0300

    Add models and create structure to save models

--- src/model.py
+++ src/model.py
@@ -2,9 +2,19 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+import os
 
-# Import your Minesweeper environment
+# Import your Minesweeper environment"
 from game_env import MinesweeperEnv
+
+models_dir = "../models/PPO"
+logdir = "../logs"
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+if not os.path.exists(logdir):
+    os.makedirs(logdir)
 
 # Create a function to train the model
 def train_minesweeper():
@@ -15,14 +25,16 @@ def train_minesweeper():
 
     env = MinesweeperEnv(graphics=False) # You can set graphics to False to run without visuals
     env.reset(seed=42)
-    # Create a PPO model
-    model = PPO("MlpPolicy", env, verbose=1)  # You can use different policies and hyperparameters
 
-    for i in range(1000):
+    # Create a PPO model
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)  # You can use different policies and hyperparameters
+
+    TIMESTEPS = 10000
+    for i in range(1,50):
         # Train the model
-        model.learn(total_timesteps=int(1e4), reset_num_timesteps=False)  # Adjust the number of training steps as needed
+        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="models")  # Adjust the number of training steps as needed
         # Save the trained model
-        model.save(f"../saved-models/minesweeper_model{i}")                                 # Wellington
+        model.save(f"{models_dir}/minesweeper_model{i * TIMESTEPS}")                                 # Wellington
 
     # Evaluate the model
     #mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=1)
